7e6d08148dbe3603b6b35dd22a2c8bbe7e831340
Abhishek <abhishrm9072@gmail.com>
2025-08-18 23:38:19 +0530

    Logic: Simple introduction to pattern programming No loops needed - just demonstrate basic output

--- basic/hello_world.py
+++ basic/hello_world.py
@@ -1,2 +1,9 @@
-for i in range (1,10):
-    print("hello world")
+# for i in range (1,10):
+#     print("hello world")
+
+
+def hello_world():
+    print("Welcome to Pattern Programming!")
+    print("Let's start with basic patterns")
+    print("*" * 20)
+hello_world()
